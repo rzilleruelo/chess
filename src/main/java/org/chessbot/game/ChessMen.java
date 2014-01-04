74a84fafbfcc85f09750bdcf7a70069b847a7186
Ricardo Zilleruelo <rzilleruelo@gmail.com>
2014-01-04 02:38:01 -0800

    Removed commented code.

--- src/main/java/org/chessbot/game/ChessMen.java
+++ src/main/java/org/chessbot/game/ChessMen.java
@@ -29,19 +29,6 @@ public abstract class ChessMen {
         return this.column;
     }
 
-//    public void move(int row, char column) {
-//        if (canMove(row, column) && shouldMove(row, column)) {
-//            this.chessBoard.grid[this.row][this.column - 'a'] = null;
-//            this.row = row;
-//            this.column = column;
-//            this.chessBoard.grid[this.row][this.column - 'a'] = this;
-//        } else {
-//            throw new IllegalArgumentException(String.format(
-//                "%s cannot move to %s%d", this.getClass().getName(), column, row
-//            ));
-//        }
-//    }
-
     public abstract boolean canMove(int row, char column);
 
 }
